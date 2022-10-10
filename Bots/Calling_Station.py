2acae0ad96479679436b4747e3f6248a3a696a5c
john1571 <johnny96jones@gmail.com>
2022-10-10 17:41:02 -0500

    #12 Add calling station

---
+++ Bots/Calling_Station.py
@@ -0,0 +1,12 @@
+from Bots import base_player as bp
+import random
+
+
+class CallingStation(bp.Player):
+    def act(self, bet, my_bet, table=None, actions=None, pot=None):  # actions = dictionary: name:(action, amount)
+        def call():
+            return bet - my_bet
+        return call()
+
+    def bot_type(self):
+        return "Calling Station"
