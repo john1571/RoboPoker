598e666f540c5465d2cc1da6f5b41f65ed92b86a
john1571 <johnny96jones@gmail.com>
2022-10-11 21:05:06 -0500

    #14 Improve interface and reduce hard coding

--- Console_Interface.py
+++ Console_Interface.py
@@ -10,7 +10,10 @@ pot = 0
 table = ""
 user_turn = False
 
-def pad_num_to_string(number, string_length, num_tabs, prefix = ''):
+column_width = 8
+column_tabs = 2
+
+def pad_num_to_string(number, string_length= column_width, num_tabs = column_tabs, prefix = ''):
     padded_string = ""
     spaces_needed = string_length
     temp = number/10
@@ -29,8 +32,26 @@ def pad_num_to_string(number, string_length, num_tabs, prefix = ''):
     tabs = ""
     for i in range(0, num_tabs):
         tabs += '\t'
+        if i == num_tabs - 2:
+            tabs += "|"
     return padded_string + prefix + str(number) + tabs
 
+
+def pad_string(string, string_length = column_width, num_tabs = column_tabs):
+    tabs = ""
+    for i in range(0, num_tabs):
+        tabs += '\t'
+        if i == num_tabs - 2:
+            tabs += "|"
+
+    if string_length < 0:
+        return "" + tabs
+    if len(string) > string_length:
+        string = string[:string_length]
+    else:
+        while len(string) < string_length:
+            string = string + " "
+    return string + tabs
 
 
 def print_status(players, bets, current_actor, pot, table, user, sleep=2):
@@ -38,54 +59,57 @@ def print_status(players, bets, current_actor, pot, table, user, sleep=2):
         if (current_actor):
             print("Round:\t" + str(current_actor.stats.rounds))
         line_1 = "Table:\t" + table.show()
-        name_line = "Names:\t\t"
-        bot_type_line = "Bots:\t\t"
-        player_chips = "Chips:\t\t"
-        player_hands = "Hands:\t\t"
-        hands_string = "Values:\t\t"
-        bets_string =  "Bets:\t\t"
-        last_round = "Last:\t\t"
-        average_win =  "Av. W.:\t\t"
-        average_loss = "Av. L.:\t\t"
-        average_delta = "Av. D.:\t\t"
-        percent_won = "Win %: \t\t"
+        name_line = pad_string("Names:")
+        bot_type_line = pad_string("Bots:")
+        player_chips = pad_string("Chips:")
+        player_hands = pad_string("Hands:")
+        hands_string = pad_string("Values:")
+        bets_string =  pad_string("Bets:")
+        last_round = pad_string("Last:")
+        average_win =  pad_string("Av. W.:")
+        average_loss = pad_string("Av. L.:")
+        average_delta = pad_string("Av. D.:")
+        percent_won = pad_string("Win %:")
         local_sleep = sleep
         for player in players:
             if player.busted:
                 continue
             if player == current_actor:
-                player_chips += ">"
+                name_line += ">"
+                name_line += pad_string(player.name, column_width - 1)
                 if player.busted or player.folded:
                     local_sleep = 0
-            name_line += player.name[:4] + ": \t\t"
-            bot_type_line += player.bot_type()[:4] + "\t\t"
+            else:
+                name_line += pad_string(player.name)
+            bot_type_line += pad_string(player.bot_type())
+
             chips_string = ""
             if player.busted:
                 chips_string = " OUT"
             else:
-                chips_string = pad_num_to_string(player.chips, 4, 2)
+                chips_string = pad_num_to_string(player.chips)
             player_chips += chips_string
+
             if player.hand:
-                player_hands += player.hand.show_for_print() + "\t\t"
-                hands_string += player.hand.get_hand_string(table)[:4] + "\t\t"
+                player_hands += player.hand.show_for_print() + pad_string("", column_width - 5)
+                hands_string += pad_string(player.hand.get_hand_string(table))
             else:
-                player_hands += "\t\t\t"
+                player_hands += pad_string("")
             if player.folded:
-                bets_string += " fold" + "\t\t"
+                bets_string += pad_string("fold")
             elif player.busted:
-                bets_string += "   out" + "\t\t"
+                bets_string += pad_string("--")
             else:
-                bet_string = pad_num_to_string(bets[player.name], 4, 2)
-                bets_string += bet_string
-            win_average = pad_num_to_string(player.stats.av_win(), 4, 2)
+                bets_string += pad_num_to_string(bets[player.name])
+            win_average = pad_num_to_string(player.stats.av_win())
             average_win += win_average
-            loss_average = pad_num_to_string(player.stats.av_loss(), 4, 2)
+            loss_average = pad_num_to_string(player.stats.av_loss())
             average_loss += loss_average
-            win_percent = pad_num_to_string(player.stats.percent_won(), 3, 2, '%')
+            win_percent = pad_num_to_string(player.stats.percent_won(), column_width - 1, column_tabs, '%')
             percent_won += win_percent
-            last_delta = pad_num_to_string(player.stats.last_delta, 4, 2)
+            last_delta = pad_num_to_string(player.stats.last_delta)
             last_round += last_delta
-            av_delta = pad_num_to_string(player.stats.av_delta(), 4 ,2)
+            av_delta = pad_num_to_string(player.stats.av_delta())
             average_delta += av_delta
         print("\n\n\n\n\n")
         if (current_actor):
