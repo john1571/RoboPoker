01fd214d00de325d4703752c301ff01dd809c86b
john1571 <johnny96jones@gmail.com>
2022-10-12 18:07:27 -0500

    #24 Allow some control using a dealer config file

---
+++ Dealer/Parse_Config.py
@@ -0,0 +1,21 @@
+import os
+
+
+config_file = os.getcwd() + "\\Dealer\\config.ini"
+
+def parse(key):
+    if not os.path.exists(config_file):
+        return False
+    with open(config_file, "r") as config:
+        line = config.readline()
+        while line:
+            key_length = len(key)
+            if len(line) < key_length:
+                continue
+            if line[:key_length] == key:
+                values = line.split("=")
+                value = values[1]
+                value = value.replace("\n", "")
+                return value
+            line = config.readline()
+        return ""
