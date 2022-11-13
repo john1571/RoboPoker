13554ee2c7ea654a9e97c8d387fb9552ec05d68a
john1571 <johnny96jones@gmail.com>
2022-11-13 15:56:00 -0600

    #35 UNIT TESTS!! * Add broken test case

--- Unit_Tests/Test_cases.py
+++ Unit_Tests/Test_cases.py
@@ -28,7 +28,23 @@ def test_round_cases():
     ending_values_2 = [
         ('Abe', 80), ('Ben', 40), ('Cob', 0), ('Dan', 20)
     ]
+    # TEST 3 ***********************************************************
+    player_dict_3 = {
+        'Abe': {'cards': ['Ad', 'As'], 'chips': 20},
+        'Ben': {'cards': ['Ah', 'Ac'], 'chips': 10},
+        'Cob': {'cards': ['3h', '3c'], 'chips': 10},
+        'Dan': {'cards': ['4h', '4c'], 'chips': 20}
+    }
+    betting_3 = {
+        0: [('Abe', 5), ('Ben', 10), ('Cob', 10), ('Dan', 10)],
+        1: [('Abe', 25), ('Ben', None), ('Cob', None), ('Dan', 10)],
+    }
+    ending_values_3 = [
+        ('Abe', 40), ('Ben', 20), ('Cob', 0), ('Dan', 0)
+    ]
     return [
         (player_dict_1, betting_1, ending_values_1),
         (player_dict_2, betting_2, ending_values_2),
+        #(player_dict_3, betting_3, ending_values_3),
+
     ]
